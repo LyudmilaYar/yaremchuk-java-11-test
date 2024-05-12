6801d4794afef43eff18c152a4f2348d8a13260d
Lyudmila Yaremchuk <milatest.rnd@gmail.com>
2024-05-12 23:52:47 +0300

    Тестовое задание 1

---
+++ src/main/java/org/example/Main1.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        long a = scanner.nextLong();
+        System.out.print("Enter b: ");
+        long b = scanner.nextLong();
+        if (a < b) {
+            System.out.println("a < b");
+        } else if (a > b) {
+            System.out.println("a > b");
+        } else {
+            System.out.println("a = b");
+        }
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (1.0 * a / b));
+    }
+}
